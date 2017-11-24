fdab4815d323fbf9560724e5257a962e4430a4a8
Song Zhou <zhousong1993@gmail.com>
2017-11-24 11:26:33 +0800

    add UIColor Category

---
+++ SZColorHex/UIColor+SZColorHex.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+SZColorHex.h
+//  SZColorHex
+//
+//  Created by Song Zhou on 24/11/2017.
+//  Copyright © 2017 Song Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (SZColorHex)
+
+@end
---
+++ SZColorHex/UIColor+SZColorHex.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+SZColorHex.m
+//  SZColorHex
+//
+//  Created by Song Zhou on 24/11/2017.
+//  Copyright © 2017 Song Zhou. All rights reserved.
+//
+
+#import "UIColor+SZColorHex.h"
+
+@implementation UIColor (SZColorHex)
+
+@end
